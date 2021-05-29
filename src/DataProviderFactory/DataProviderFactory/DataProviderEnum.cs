acb00d57175c13afd19473d9cc004bef9b5603d7
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-05-29 17:10:14 -0400

    Added console app that reads db

---
+++ src/DataProviderFactory/DataProviderFactory/DataProviderEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProviderFactory
+{
+  enum DataProviderEnum
+  {
+  //OleDb is Windows only and is not supported in .NET Core
+  SqlServer,
+#if PC
+    OleDb,
+#endif
+    Odbc
+  }
+}
