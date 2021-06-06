70bdfa8d5312fc803913b878d06454a4360c1d44
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-06-06 05:13:54 -0400

    Added BaseEntity configuration

---
+++ src/Chapter23_AllProjects/AutoLot.Dal/EfStructures/Configurations/BaseEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AutoLot.Model.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoLot.Dal.EfStructures.Configurations
+{
+  public class BaseEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity
+  {
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+      builder.HasKey(e => e.Id);
+
+      builder.Property(e => e.TimeStamp)
+        .IsRowVersion()
+        .IsConcurrencyToken();
+    }
+  }
+}
