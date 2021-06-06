b157c6891467d314134e10a6b0f87d82b806fecd
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-06-06 05:00:52 -0400

    Added Person owned entity

---
+++ src/Chapter23_AllProjects/AutoLot.Model/Entities/Owned/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLot.Model.Entities.Owned
+{
+  public class Person
+  {
+    public string FirstName { get; set; } = "New";
+    public string LastName { get; set; } = "Customer";
+    public string? FullName { get; set; }
+  }
+}
