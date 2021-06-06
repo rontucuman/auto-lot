e669081f6439ae396b2719d7e5940337512a7e52
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-06-05 21:11:02 -0400

    Added DBContext

---
+++ src/AutoLot.Samples/AutoLot.Samples/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoLot.Samples.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoLot.Samples
+{
+  public partial class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Car> Cars { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    static void SampleSaveChanges()
+    {
+      var context = new ApplicationDbContextFactory().CreateDbContext(null);
+      context.SaveChanges();
+    }
+  }
+}
---
+++ src/AutoLot.Samples/AutoLot.Samples/ApplicationDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AutoLot.Samples
+{
+  public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+  {
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+      var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+      var connectionString = @"Data Source=(localdb)\\mssqllocaldb;Integrated Security=true; Initial Catalog=AutoLot";
+
+      optionsBuilder.UseSqlServer(connectionString);
+      Console.WriteLine(connectionString);
+      return new ApplicationDbContext(optionsBuilder.Options);
+    }
+  }
+}
