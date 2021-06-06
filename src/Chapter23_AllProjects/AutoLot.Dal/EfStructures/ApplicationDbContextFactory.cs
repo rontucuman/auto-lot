68cfcb30b7414d4f0f8cec834e052c361b246cd9
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-06-05 22:39:30 -0400

    Switched to Code First

---
+++ src/Chapter23_AllProjects/AutoLot.Dal/EfStructures/ApplicationDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AutoLot.Dal.EfStructures
+{
+  public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+  {
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+      var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+      string connectionString = @"Data Source=(localdb)\\mssqllocaldb;Integrated Security=true; Initial Catalog=AutoLot";
+
+      optionsBuilder.UseSqlServer(connectionString);
+
+      return new ApplicationDbContext(optionsBuilder.Options);
+    }
+  }
+}
